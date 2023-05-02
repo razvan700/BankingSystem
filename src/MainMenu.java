df4e21c632c531ee9de713bb158cb2050c8e4767
razvan700 <110129667+razvan700@users.noreply.github.com>
2023-05-02 12:55:37 +0300

    Created the first two drop down menus in the UI

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,2 +1,28 @@
-public class MainMenu {
+import javax.swing.*;
+
+public class MainMenu extends JFrame {
+    private JPanel mainPanel;
+    private JComboBox comboBox1;
+
+    public MainMenu() {
+        // set up frame properties
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Main Menu");
+
+        JPanel contentPane = new JPanel();
+        setContentPane(contentPane);
+        contentPane.add(mainPanel); // add mainPanel to the content pane
+
+        // pack and display frame
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+
+        menu.setSize(1920,1080);
+        menu.setVisible(true);
+    }
 }
